9e116d01890ce4a8bd3d8b6e94cffb12a22fdfb0
Nathan Andrade <onathan_andrade@outlook.com>
2021-07-02 16:16:55 -0300

    Adição H2

---
+++ src/main/java/com/spring/projectSpring/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.spring.projectSpring.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.projectSpring.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "teste1");
+		Categoria cat2 = new Categoria(2, "teste2");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
+	}
+	
+}
